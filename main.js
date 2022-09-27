5c90b17dfa1fe3388eeaa87af0c3d8293fdf98cf
brandonTsai <brandonya125@gmail.com>
2022-09-27 10:11:42 -0700

    Demo two ways to select element by its id in main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert('Connected!!')
+const container1 = document.getElementById('container')
+const container2 = document.querySelector('#container')
+
+console.log(
+  'This is container1', container1,
+  'This is container2', container2,
+  'These two are identical', container1 === container2
+)
