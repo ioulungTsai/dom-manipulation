62b042bdb750ec8ac960c80c1d3a30cf9d52744c
brandonTsai <brandonya125@gmail.com>
2022-09-27 10:26:30 -0700

    Add h3 with blue text in the container div

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const container = document.getElementById('container')
 const pTag = document.createElement('p')
+const h3Tag = document.createElement('h3')
 
 pTag.style.color = 'red'
 pTag.textContent = `Hey I'm red!`
 
+h3Tag.style.color = 'blue'
+h3Tag.textContent = `I'm a blue h3!`
+
 container.appendChild(pTag)
+container.appendChild(h3Tag)
